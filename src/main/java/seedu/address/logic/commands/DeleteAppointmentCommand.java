fae12b6a9d61bb35d2bd5552184503b39847a683
jxkhoo8@gmail.com <jxkhoo8@gmail.com>
2026-03-11 20:18:06 +0800

    Add DeleteAppointmentCommand skeleton
    No command exists to remove a patient's appointment from the system.
    
    Doctors need a way to cancel or clear a scheduled appointment for
    a patient without deleting the entire patient record.
    
    Let's add the DeleteAppointmentCommand class with command word
    deleteappt, which accepts a patient index as its only parameter.
    
    Appointment-specific logic (empty appointment check and clearing the
    appointment field) is stubbed with TODO comments as it depends on
    the Appointment feature PR that has not yet been merged.

---
+++ src/main/java/seedu/address/logic/commands/DeleteAppointmentCommand.java
@@ -0,0 +1,102 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Deletes the appointment of the person identified by the displayed index from the address book.
+ */
+public class DeleteAppointmentCommand extends Command {
+
+    public static final String COMMAND_WORD = "deleteappt";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the appointment of the person identified "
+            + "by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_APPOINTMENT_SUCCESS =
+            "Deleted appointment for Person: %1$s";
+
+    public static final String MESSAGE_NO_APPOINTMENT =
+            "This person does not have an appointment to delete.";
+
+    private final Index targetIndex;
+
+    public DeleteAppointmentCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(targetIndex.getZeroBased());
+
+        // TODO: Uncomment once Cavan's Appointment PR is merged (adds getAppointment() to Person)
+        // if (personToEdit.getAppointment().isEmpty()) {
+        //     throw new CommandException(MESSAGE_NO_APPOINTMENT);
+        // }
+
+        // TODO: Uncomment once Cavan's Appointment PR is merged (adds 6-param Person constructor)
+        // Person updatedPerson = new Person(
+        //         personToEdit.getName(),
+        //         personToEdit.getPhone(),
+        //         personToEdit.getEmail(),
+        //         personToEdit.getAddress(),
+        //         personToEdit.getTags(),
+        //         null
+        // );
+
+        // TODO: Remove this 5-param Person constructor once Cavan's Appointment PR is merged
+        Person updatedPerson = new Person(
+                personToEdit.getName(),
+                personToEdit.getPhone(),
+                personToEdit.getEmail(),
+                personToEdit.getAddress(),
+                personToEdit.getTags()
+        );
+
+        model.setPerson(personToEdit, updatedPerson);
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(
+                String.format(MESSAGE_DELETE_APPOINTMENT_SUCCESS, Messages.format(updatedPerson)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteAppointmentCommand)) {
+            return false;
+        }
+
+        DeleteAppointmentCommand otherCommand = (DeleteAppointmentCommand) other;
+        return targetIndex.equals(otherCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
